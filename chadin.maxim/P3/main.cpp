5727e6b16898419597357f3eeb5dc454f9356b5f
maxchadin <maxchadin080418@gmail.com>
2025-11-15 23:50:00 +0300

    commit 1

--- chadin.maxim/P3/main.cpp
+++ chadin.maxim/P3/main.cpp
@@ -1,2 +1,27 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+namespace chadin
+{
+  std::istream & readArr(std::istream & input, int * arr, size_t rows, size_t cols);
+  std::ostream & writeArr(std::ostream & output, const int * arr, size_t rows, size_t cols, int result);
+  void increaseElements(int * arr, size_t rows, size_t cols);
+  int countDiagonals(const int * arr, size_t rows, size_t cols);
+}
+
+std::istream & chadin::readArr(std::istream & input, int * arr, size_t rows, size_t cols)
+{
+}
+
+std::ostream & chadin::writeArr(std::ostream & output, const int * arr, size_t rows, size_t cols, int result)
+{
+}
+
+void chadin::increaseElements(int * arr, size_t rows, size_t cols)
+{
+}
+
+int chadin::countDiagonals(const int * arr, size_t rows, size_t cols)
+{
+}

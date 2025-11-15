c82385db88bc4588c05e71a5f09d09ac70732bb7
maxchadin <maxchadin080418@gmail.com>
2025-11-15 23:56:44 +0300

    commit 2

--- chadin.maxim/P3/main.cpp
+++ chadin.maxim/P3/main.cpp
@@ -12,6 +12,20 @@ namespace chadin
 
 std::istream & chadin::readArr(std::istream & input, int * arr, size_t rows, size_t cols)
 {
+  size_t number = 0;
+  for (size_t i = 0; i < rows * cols && input >> arr[i]; i++)
+  {
+    ++number;
+  }
+  if (!input)
+  {
+    std::cerr << "Incorrect input" << "\n";
+  }
+  else if (rows * cols > number)
+  {
+    std::cerr << "Not enough elements" << "\n";
+  }
+  return input;
 }
 
 std::ostream & chadin::writeArr(std::ostream & output, const int * arr, size_t rows, size_t cols, int result)
